98f4012037726671a03360b4a4c423d3d97806f9
nurel26-1 <nlomurzakov@gmail.com>
2023-04-27 19:42:49 +0600

    hw-3

--- product/views.py
+++ product/views.py
@@ -22,7 +22,7 @@ def product_list_api_view(request):
         return Response(status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail_api_view(request, id):
     try:
         product = Product.objects.get(id=id)
@@ -31,7 +31,7 @@ def product_detail_api_view(request, id):
     if request.method == 'GET':
         data_dict = ProductSerializer(product, many=False).data
         return Response(data=data_dict)
-    elif request.method == 'POST':
+    elif request.method == 'PUT':
         product.title = request.data.get('title')
         product.description = request.data.get('description')
         product.price = request.data.get('price')
@@ -58,7 +58,7 @@ def review_list_api_view(request):
         return Response(status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def review_detail_api_view(request, id):
     try:
         review = Review.objects.get(id=id)
@@ -67,7 +67,7 @@ def review_detail_api_view(request, id):
     if request.method == 'GET':
         data_dict = ReviewSerializer(review, many=False).data
         return Response(data=data_dict)
-    elif request.method == 'POST':
+    elif request.method == 'PUT':
         review.text = request.data.get('text')
         review.stars = request.data.get('stars')
         review.product_id = request.data.get('product_id')
@@ -92,7 +92,7 @@ def category_list_api_view(request):
         return Response(status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def category_detail_api_view(request, id):
     try:
         category = Category.objects.get(id=id)
@@ -101,7 +101,7 @@ def category_detail_api_view(request, id):
     if request.method == 'GET':
         data_dict = CategorySerializer(category, many=False).data
         return Response(data=data_dict)
-    elif request.method == 'POST':
+    elif request.method == 'PUT':
         category.name = request.data.get('name')
         category.save()
         return Response(status=status.HTTP_200_OK)
